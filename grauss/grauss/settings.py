68333e1dfcba2aa929a9bd2ea66e0f734fc6d37c
Miguel Diaz <miguel.diaz@balsain.io>
2022-11-13 00:46:38 +0100

    add postgres config

--- grauss/grauss/settings.py
+++ grauss/grauss/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'grauss.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'graussdb',
+        'USER': 'graussuser',
+        'PASSWORD': 'grausspasswd',
+        'HOST': 'postgres-grauss',
+        'PORT': '5432',
     }
 }
 
